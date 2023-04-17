e26992597b5b37215a80921fa2bd23633222f70b
maulanardy <38031045+maulanardy@users.noreply.github.com>
2023-04-17 08:11:45 +0700

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -l
 
 codeql database analyze --format=sarif-latest --output=./ query
-echo "Hello $1"
+# echo "Hello $1"
 echo "Hello bang"
 time=$(date)
 echo "time=$time" >> $GITHUB_OUTPUT
