e8ffa060cf7d217f4cab3fa17997193278147fe5
maulanardy <maulanardy.dev@gmail.com>
2023-04-21 05:31:11 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/sh -l
 
 USERNAME=$1
+QUERYPATH=$2
+OUTPUTPATH=$3
 
 cd /action
 echo "creating docker image for user: $USERNAME"
 
 # here we can make the construction of the image as customizable as we need
 # and if we need parameterizable values it is a matter of sending them as inputs
-docker build -t qcve/action --build-arg USERNAME="$USERNAME" . && docker run -v 'd:/Works/Moonlay/Shannon/qcve/codeql-sample-action/output':/opt/output qcve/action
+docker build -t qcve/action --build-arg USERNAME="$USERNAME" . && docker run -v $OUTPUTPATH:/opt/output $QUERYPATH:/opt/queries qcve/action
